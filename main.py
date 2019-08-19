c3855ead25651195d9c41142458e340422416c12
danbar0 <35388915+danbar0@users.noreply.github.com>
2019-08-18 21:21:17 -0500

    Added base project files

---
+++ main.py
@@ -0,0 +1,68 @@
+import numpy
+import scipy.special
+
+#neural network class
+class NeuralNetwork:
+    def __init__(self, input_nodes, hidden_nodes, output_nodes, learning_rate=0.5):
+        self.input_nodes = input_nodes
+        self.hidden_nodes = hidden_nodes
+        self.output_nodes = output_nodes
+        self.learning_rate = learning_rate
+
+        self.input_to_hidden_weights = numpy.random.normal(0.0, pow(self.input_nodes, -0.5),
+                                                        (self.hidden_nodes,self.input_nodes))
+        self.hidden_to_output_weights = numpy.random.normal(0.0, pow(self.hidden_nodes, -0.5),
+                                                         (self.output_nodes, self.hidden_nodes))
+
+        self.activationFunction = lambda x: scipy.special.expit(x)
+
+    def train(self, inputs_list, targets_list):
+        # convert inputs list to 2d array
+        inputs = numpy.array(inputs_list, ndmin=2).T
+        targets = numpy.array(targets_list, ndmin=2).T
+
+        # calculate sigansl into hidden layer
+        hidden_inputs = numpy.dot(self.input_to_hidden_weights, inputs)
+        # calculate the signals emerging from hidden layer
+        hidden_outputs = self.activationFunction(hidden_inputs)
+
+        # calculate signals into final output layer
+        final_inputs = numpy.dot(self.hidden_to_output_weights, hidden_outputs)
+        # calculate the signals emerging from final output layer
+        final_outputs = self.activationFunction(final_inputs)
+
+        output_errors = targets - final_outputs
+
+    def query(self, inputs_list):
+        # convert inputs list to 2d array
+        inputs = numpy.array(inputs_list, ndmin=2).T
+
+        # calculate signals into hidden layers
+        hidden_inputs = numpy.dot(self.input_to_hidden_weights, inputs)
+
+        # calculate the signals emerging from hidden layer
+        hidden_outputs = self.activationFunction(hidden_inputs)
+
+        # calculate signals into final output layer
+        final_inputs = numpy.dot(self.hidden_to_output_weights, hidden_outputs)
+
+        # calculate the signals emerging from final output layer
+        final_outputs = self.activationFunction(final_inputs)
+
+        return final_outputs
+
+
+def main():
+    input_nodes = 3
+    hidden_nodes = 3
+    output_nodes = 3
+    learning_rate = 0.5
+
+    network = NeuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
+
+    print(network.query([1.0, 0.5, -1.5]))
+
+
+if __name__ == '__main__':
+    main()
+    quit()
