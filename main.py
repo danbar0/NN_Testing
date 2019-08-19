c883f3520b99844ede0ab05bab3e2058004ca78c
danbar0 <35388915+danbar0@users.noreply.github.com>
2019-08-18 21:33:21 -0500

    Change to test git integration

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy
 import scipy.special
 
-#neural network class
+
 class NeuralNetwork:
     def __init__(self, input_nodes, hidden_nodes, output_nodes, learning_rate=0.5):
         self.input_nodes = input_nodes
